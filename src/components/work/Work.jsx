4408d37714f0d067490edc77a63000de0bfc97ce
onukwilip <onukwilip@gmail.com>
2023-04-06 16:11:45 +0100

    📝🖋🔨 Modified the Work section, removed the circles and displayed the organizations ive worked with/for

--- src/components/work/Work.jsx
+++ src/components/work/Work.jsx
@@ -1,96 +1,163 @@
-import React from "react";
+import React, { useState } from "react";
 import "./work.css";
-import upwork from "../../img/Upwork.png";
-import fiverr from "../../img/fiverr.png";
-import kwork from "../../img/kwork.png";
-import truelancer from "../../img/truelancer2.png";
-import freelancer from "../../img/freelancer.png";
+import { useInView } from "react-intersection-observer";
+import { useAnimation, motion } from "framer-motion";
+import { useEffect } from "react";
+
+class PlaceWorkedClass {
+  constructor(name, title, date, description, link) {
+    this.name = name;
+    this.title = title;
+    this.date = date;
+    this.desc = description;
+    this.link = link;
+  }
+}
+
+const placesWorked = [
+  new PlaceWorkedClass(
+    "GOPack",
+    "Founder",
+    ["2023/03/26", null],
+    "I founded GOPack, an open-source javascript library which helps developers with little to no experience of Webpack build and bundle their projects into static files",
+    "https://gopack.vercel.app"
+  ),
+  new PlaceWorkedClass(
+    "Momas LTD Nigeria",
+    "Software developer",
+    ["2022/07/26", null],
+    [
+      "Developed software and web applications using the .NET framework, JavaScript framework and libraries, the likes of React Js, Next Js, etc...and MSSQL database.",
+      "Developed a payment web application alongside fellow developers, in order for their customers to pay for the organisation's resources e.g Power/energy. The application was developed using React Js, SCSS, Java (JAX-RS) and MSSQL database.",
+      "Developed a real-time tracking web application alongside fellow team members which will be deployed into the military where, registered users across the field will be tracked via the mobile application installed on their Walkie talkies. The application is in an early stage and is being developed using React Js, SCSS, Docker, Node Js Web Socket/MQTT, Google Maps API, MSSQL, Spring boot, etc",
+    ],
+    "https://www.memmcol.com"
+  ),
+  new PlaceWorkedClass(
+    "Fiverr",
+    "Freelancer",
+    ["2022/10/01", null],
+    [
+      "Worked as a freelancer at Fiverr, developed web applications and software for potential clients",
+      "Developed an auction application for a client on Fiverr. Users can signup, bid for products, post/update/delete products, etc. The application involves REST API integration, JWT authentication, email verification, microservices, Docker, etc.",
+    ],
+    "https://www.fiverr.com/go_industries"
+  ),
+  new PlaceWorkedClass(
+    "Upwork",
+    "Freelancer",
+    ["2022/10/01", null],
+    "Worked as a freelancer at Upwork, developed web applications and software for potential clients",
+    "https://www.upwork.com/freelancers/~01e414823e14f1cdde"
+  ),
+];
 
 const Work = () => {
+  const [selectedPlace, setSelectedPlace] = useState(placesWorked[0]);
+  const [ref, inView] = useInView();
+  const control = useAnimation();
+  const leftVariants = {
+    far: {
+      opacity: 0,
+      x: -500,
+    },
+    current: {
+      opacity: 1,
+      x: 0,
+    },
+  };
+  const rightVariants = {
+    far: {
+      opacity: 0,
+      x: 500,
+    },
+    current: {
+      opacity: 1,
+      x: 0,
+    },
+  };
+
+  useEffect(() => {
+    if (inView) {
+      control.start("current");
+    } else {
+      control.start("far");
+    }
+  }, [control, inView]);
+
   return (
     <section>
-      <div className="row w-main" id="Work">
-        <div className="col-xl-6 w-l-side">
+      <div className="row w-main" id="Work" ref={ref}>
+        <motion.div
+          className="col-xl-6 w-l-side"
+          variants={leftVariants}
+          initial="far"
+          animate={control}
+        >
           <div>
             <h2>Works for all these</h2>
             <h1 className="highlight">Brands and Clients</h1>
             <p className="text">
-              I am a freelancer and at top freelancing companies in the world!
-              E.g Fiverr, Upwork, Kwork, Freelancer, Truelancer and so on...
+              I am/was a software developer at various organizations, clients
+              and platforms.
             </p>
             <br />
             <br />
-            <a href="#Contact">
+            <a href="#Contact" className="action">
               <button>Hire me...</button>
             </a>
           </div>
-        </div>
-        <div className="col-xl-6 w-r-side">
-          <div className="bg">
-            <div className="bg-circle bg-circle-2"></div>
-            <div className="bg-circle bg-circle-1"></div>
-            <div className="circle">
-              <a
-                href="https://www.truelancer.com/freelancer/princeonukwili"
-                target="_blank"
+        </motion.div>
+        <motion.div
+          className="col-xl-6 w-r-side"
+          variants={rightVariants}
+          initial="far"
+          animate={control}
+        >
+          <ul className="places-worked-list">
+            {placesWorked?.map((eachPlace, i) => (
+              <li
+                key={i}
+                className={
+                  selectedPlace?.name === eachPlace?.name ? "active" : ""
+                }
+                onClick={() => setSelectedPlace(eachPlace)}
               >
-                <div
-                  className="img-circle"
-                  style={{
-                    top: "-3rem",
-                    left: "7rem",
-                  }}
+                {eachPlace?.name?.split(" ")[0] || eachPlace?.name}
+              </li>
+            ))}
+          </ul>
+          <div className="content">
+            <div className="header">
+              <em>
+                {selectedPlace?.title}{" "}
+                <a
+                  href={selectedPlace?.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  <img src={freelancer} alt="" />
-                </div>
-              </a>
-              <a
-                href="https://www.upwork.com/freelancers/~01e414823e14f1cdde"
-                target="_blank"
-              >
-                <div
-                  className="img-circle"
-                  style={{
-                    top: "7rem",
-                    left: "-3rem",
-                  }}
-                >
-                  <img src={upwork} alt="" />
-                </div>
-              </a>
-              <a
-                href="https://www.freelancer.com/u/PrinceAgozie"
-                target="_blank"
-              >
-                <div
-                  className="img-circle"
-                  style={{
-                    bottom: "-3rem",
-                    left: "7rem",
-                  }}
-                >
-                  <img src={truelancer} alt="" />
-                </div>
-              </a>
-              <a href="https://kwork.com/user/onukwilip" target="_blank">
-                <div
-                  className="img-circle"
-                  style={{
-                    top: "7rem",
-                    right: "-3rem",
-                  }}
-                >
-                  <img src={kwork} alt="" />
-                </div>
-              </a>
-              <a href=" https://www.fiverr.com/go_industries" target="_blank">
-                <div className="img-center">
-                  <img src={fiverr} alt="" />
-                </div>
-              </a>
+                  @{selectedPlace?.name}
+                </a>
+              </em>
+              <code>
+                {selectedPlace?.date[0]} - {selectedPlace?.date[1] || "Present"}
+              </code>
+            </div>
+            <div className="desctiption">
+              {Array.isArray(selectedPlace?.desc) ? (
+                <>
+                  <ul>
+                    {selectedPlace?.desc?.map((eachDesc, i) => (
+                      <li>{eachDesc}</li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                selectedPlace?.desc
+              )}
             </div>
           </div>
-        </div>
+        </motion.div>
       </div>
     </section>
   );

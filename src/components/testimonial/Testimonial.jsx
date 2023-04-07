d1102ee61124c03e5381fdef8997fa0ed0e41219
onukwilip <onukwilip@gmail.com>
2023-04-07 16:58:24 +0100

    Modifying the testimonial component

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -6,40 +6,35 @@ import Slider from "react-slick";
 import profile1 from "../../img/profile1.jpg";
 import profile2 from "../../img/profile2.jpg";
 import profile3 from "../../img/profile3.jpg";
-import profile4 from "../../img/profile4.jpg";
-import profile5 from "../../img/profile5.jpg";
 
 const Testimonial = () => {
   const testimonial = [
     {
       id: 1,
       image: profile1,
+      name: "Olusegun Adebumiti",
+      twitterLink: "",
+      linkedinLink: "",
       comment:
         "A robust congratulations to Prince for a job well done. \n We've been trying to put together a functional website since I began my practice in April of 2012. I am happy to say we finally hired Prince and his team, and they've worked closely with us throughout the process, staying on task, on target, and on budget. I also appreciate their quick and courteous responses. I highly recommend their service!",
     },
     {
       id: 2,
       image: profile2,
+      twitterLink: "",
+      name: "Tunde Tiamiyu",
+      linkedinLink: "",
       comment:
         "We thank Prince and his team for the wonderful job in helping us develop our program. Everyone was professional, excellent and hard working. Thanks to them, we were able to achieve our goal on time, and we look forward to continue working with them in the future.",
     },
     {
       id: 3,
       image: profile3,
+      twitterLink: "",
+      name: "Badmus Sodiq",
+      linkedinLink: "",
       comment:
         "We would like to express our satisfaction on the co-operation regarding the development of our web application. Prince and the development team did a very professional job. We are satisfied with the solution given to us and with the communication flow through the project. We would like to recommend Prince and his development team. We look forward to working with them in future projects.",
-    },
-    {
-      id: 4,
-      image: profile4,
-      comment:
-        " I wanted to take a moment to thank you for the services your team has provided. Your team has been a pleasure to work with, professional and timely. The only delay in work that we have experienced has been due to our own lack of organization managing our projects, not yours. Job well done and I hope we can continue to grow together.",
-    },
-    {
-      id: 5,
-      image: profile5,
-      comment:
-        " I can't say enough about the excellent work that Prince and his team has done on our website. They took a below-average website and transformed it into an appealing and informative website. It was an absolute pleasure to work with them. The designer listened to my thoughts and suggestions and far surpassed my expectations. There were minor problems along the way; these were sorted out quickly and efficiently. We are looking forward to working with them on future projects. Anyone seeking a reference can call me anytime. You would be extremely unwise to overlook such a dedicated and professional team.",
     },
   ];
 
@@ -77,11 +72,22 @@ const Testimonial = () => {
       </section>
       <div>
         <Slider {...settings} className="slider">
-          {testimonial.map((eachProject) => {
+          {testimonial.map((eachTestimonial) => {
             return (
-              <div className="testimonial" key={eachProject.id} align="center">
-                <img src={eachProject.image} alt="" />
-                <p>{eachProject.comment}</p>
+              <div className="testimonial" key={eachTestimonial.id}>
+                <div className="img-container">
+                  <img src={eachTestimonial?.image} alt="" />
+                  <em>{eachTestimonial?.name}</em>
+                  <div className="actions">
+                    <a href={eachTestimonial?.twitterLink} target="_blank">
+                      <i className="fa-brands fa-twitter"></i>
+                    </a>
+                    <a href={eachTestimonial?.linkedinLink} target="_blank">
+                      <i className="fa-brands fa-linkedin"></i>
+                    </a>
+                  </div>
+                </div>
+                <p>{eachTestimonial.comment}</p>
               </div>
             );
           })}
